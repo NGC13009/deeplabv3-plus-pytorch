4e2614abadf0dee3fb9b69447577d151a6161a4b
NGC13009 <ngc1300@126.com>
2023-04-15 16:37:13 +0800

    Update deeplabv3_training.py
    这里应该是"长 或 宽 不相等, 则需要缩放(线性插值)为一样的尺寸才能比较". 
    之前是"和"
    如果是"和"的话, 那么长相等但是宽不相等就不会缩放, 那么最后label和feature无法对齐, 应该是有问题的.
    如果我以为的是对的, 那么合并一下?

--- nets/deeplabv3_training.py
+++ nets/deeplabv3_training.py
@@ -6,7 +6,7 @@ from torch import nn
 def CE_Loss(inputs, target, cls_weights, num_classes=21):
     n, c, h, w = inputs.size()
     nt, ht, wt = target.size()
-    if h != ht and w != wt:
+    if h != ht or w != wt:
         inputs = F.interpolate(inputs, size=(ht, wt), mode="bilinear", align_corners=True)
 
     temp_inputs = inputs.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
@@ -18,7 +18,7 @@ def CE_Loss(inputs, target, cls_weights, num_classes=21):
 def Focal_Loss(inputs, target, cls_weights, num_classes=21, alpha=0.5, gamma=2):
     n, c, h, w = inputs.size()
     nt, ht, wt = target.size()
-    if h != ht and w != wt:
+    if h != ht or w != wt:
         inputs = F.interpolate(inputs, size=(ht, wt), mode="bilinear", align_corners=True)
 
     temp_inputs = inputs.transpose(1, 2).transpose(2, 3).contiguous().view(-1, c)
@@ -35,7 +35,7 @@ def Focal_Loss(inputs, target, cls_weights, num_classes=21, alpha=0.5, gamma=2):
 def Dice_loss(inputs, target, beta=1, smooth = 1e-5):
     n, c, h, w = inputs.size()
     nt, ht, wt, ct = target.size()
-    if h != ht and w != wt:
+    if h != ht or w != wt:
         inputs = F.interpolate(inputs, size=(ht, wt), mode="bilinear", align_corners=True)
         
     temp_inputs = torch.softmax(inputs.transpose(1, 2).transpose(2, 3).contiguous().view(n, -1, c),-1)
